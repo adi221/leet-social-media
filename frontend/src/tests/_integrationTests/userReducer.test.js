c45a9a85674123c038a44132e9a2662759341508
adi221 <adimizrahi421@gmail.com>
2021-06-14 23:48:05 +0300

    Added more userReducer test

--- frontend/src/tests/_integrationTests/userReducer.test.js
+++ frontend/src/tests/_integrationTests/userReducer.test.js
@@ -1,7 +1,41 @@
-import { login } from '../../actions/userActions';
 import { userLoginReducer } from '../../reducers/userReducers';
 
 test('returns initial state when no action is passed', () => {
   const newState = userLoginReducer(undefined, {});
   expect(newState).toEqual({ userInfo: null });
+});
+
+test('returns expected state when USER_LOGIN_SUCCESS is passed', () => {
+  const payload = {
+    _id: '1',
+    username: 'test',
+    name: 'test name',
+    email: 'test@example.com',
+    profileImage: 'test.jpg',
+    description: 'test description',
+    token: 'token123',
+  };
+  const newState = userLoginReducer(undefined, {
+    type: 'USER_LOGIN_SUCCESS',
+    payload,
+  });
+
+  expect(newState).toEqual({
+    userInfo: payload,
+    loading: false,
+  });
+});
+
+test('returns expected state when USER_LOGIN_FAIL is passed', () => {
+  const error = { success: false, message: 'Invalid email or password' };
+  const newState = userLoginReducer(undefined, {
+    type: 'USER_LOGIN_FAIL',
+    payload: error.message,
+  });
+
+  expect(newState).toEqual({
+    error: error.message,
+    userInfo: null,
+    loading: false,
+  });
 });
